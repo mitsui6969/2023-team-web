e73b35eeadecc94fe0be130ab19eb6e27cde6cfd
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-01-26 23:26:00 +0900

    ヘッダーとサイドバーのファイル追加

---
+++ src/conponents/Header.jsx
@@ -0,0 +1,18 @@
+// import { useContext } from "react";
+
+export default function Header() {
+    // const {isAuth, setIsAuth} = useContext(AuthFlagContext);
+    // console.log(isAth);
+
+    return (
+        <>
+            <p>header</p>
+
+            <nav>
+                <ul>
+                    <link href="/src/App.jsx">Reactテンプレート</link>
+                </ul>
+            </nav>
+        </>
+    )
+}
