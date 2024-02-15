5f96fac7db008c07c46d96cc04a8b4359eb084c5
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-02-15 23:19:16 +0900

    エラー解消

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './styles/App.css'
 import './styles/global.css'
-import Sidebar from "./stories/sidebar"
+import Sidebar from "./stories/sidebar.jsx"
 
 
 function App() {
 
   return (
     <>
-      <div className='App'>
+      <div className='side'>
         <Sidebar />
       </div>
 
