e9342aee176eee34c509cdb87badb4bf12f99b81
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-02-20 00:41:43 +0900

    todoリストの雛型

--- src/task.jsx
+++ src/task.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles/task.css'
 
 export const Task = () => {
+    const [tasks, setTasks] = useState(["task1", "task2"]);
+    
+    const Task = ({task}) => {
+        return <div>{task}</div>
+    }
+
     return (
-        <div>task</div>
+        <>
+            <div>task</div>
+            <input type='text'/>
+            <button className='addButton'>追加</button>
+            <button className='delButton'>削除</button>
+            <div>残りのタスク:0</div>
+            { tasks.map((task) => <Task task={task} key={task} />) }
+        </>
     )
 }
+
