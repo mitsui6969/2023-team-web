8e1b3bcd9cc6ebe962a932e211797ea93ed45bc8
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-02-28 17:51:41 +0900

    fullcalendar追加

--- src/karender.jsx
+++ src/karender.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import './styles/karendar.css'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 export const Karender = () => {
     return (
-        <div>karender</div>
+        <>
+            <div>karender</div>
+            <div id='fc'>
+                <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+            </div>
+        </>
     )
 }
